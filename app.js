6649261208d3407ce569243e557f7cbc385eb196
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-25 11:51:23 +0900

    Feat: Login

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 5000;
 
 const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,6 +20,13 @@ const userSchema = new mongoose.Schema({
   id: String,
   password: String,
   group: String,
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
@@ -39,6 +49,34 @@ app.post('/register', async (req, res) => {
     res.status(201).json({ message: '사용자 등록 성공' });
   } catch (error) {
     res.status(400).json({ message: '사용자 등록 실패', error: error.message });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { id, password } = req.body;
+    const user = await User.findOne({ id });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: '아이디 또는 비밀번호가 일치하지 않습니다.' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ message: '아이디 또는 비밀번호가 일치하지 않습니다.' });
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    res.status(200).json({ message: '로그인 성공', token });
+  } catch (error) {
+    res.status(500).json({ message: '로그인 실패', error: error.message });
   }
 });
 
