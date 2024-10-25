ba4b9cf23e3bcb6a54c87ff5e2eb59977d4d1e42
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-25 16:39:27 +0900

    Feat: 게시물 CRUD

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,7 @@ exports.login = async (req, res) => {
         .json({ message: '아이디 또는 비밀번호가 일치하지 않습니다.' });
     }
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
 
---
+++ routes/postRoutes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const jwtMiddleware = require('../middleware/jwtMiddleware');
+const Post = require('../models/Post');
+
+// 게시글 조회
+router.get('/', async (req, res) => {
+  const posts = await Post.find().populate('author');
+  res.json(posts);
+});
+
+// 게시글 생성
+router.post('/', jwtMiddleware, async (req, res) => {
+  const { title, content, devDependencies } = req.body;
+
+  const post = new Post({
+    title,
+    content,
+    author: req.user._id,
+    devDependencies,
+  });
+  await post.save();
+  res.json(post);
+});
+
+router.put('/:id', jwtMiddleware, async (req, res) => {
+  const { title, content, tags, devDependencies } = req.body;
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    // 게시글 작성자와 토큰의 사용자 ID 비교
+    if (post.author.toString() !== req.user._id) {
+      return res.status(403).json({ message: 'Not authorized' });
+    }
+
+    post.title = title;
+    post.content = content;
+    post.tags = tags;
+    post.devDependencies = devDependencies;
+
+    const updatedPost = await post.save();
+    res.json(updatedPost);
+  } catch (error) {
+    console.error('Error updating post:', error);
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).json({ message: 'Invalid post ID' });
+    }
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+});
+
+// 게시글 삭제 라우트
+router.delete('/:id', jwtMiddleware, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    // 게시글 작성자와 토큰의 사용자 ID 비교
+    if (post.author.toString() !== req.user._id) {
+      return res.status(403).json({ message: 'Not authorized' });
+    }
+    await Post.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Post deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
