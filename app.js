b626a11a9fa3e02319a6f26c840af4f4e56b87b0
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-25 14:45:37 +0900

    Refactor: 디렉토리 구조 적용

--- app.js
+++ app.js
@@ -1,91 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const connectDB = require('./config/database');
+const corsMiddleware = require('./middleware/corsMiddleware');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 const port = 5000;
 
-const MONGODB_URI = process.env.MONGODB_URI;
-const JWT_SECRET = process.env.JWT_SECRET;
+connectDB(process.env.MONGODB_URI);
 
-mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB 연결 성공'))
-  .catch((err) => console.error('MongoDB 연결 실패:', err));
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  id: String,
-  password: String,
-  group: String,
-});
-
-userSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
-
-const User = mongoose.model('User', userSchema);
-
-let corsOptions = {
-  origin: ['http://localhost:5173'],
-};
-
-app.use(cors(corsOptions));
+app.use(corsMiddleware);
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json('welcome to DEVIEW server');
 });
 
-app.post('/register', async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).json({ message: '사용자 등록 성공' });
-  } catch (error) {
-    res.status(400).json({ message: '사용자 등록 실패', error: error.message });
-  }
-});
-
-app.post('/login', async (req, res) => {
-  try {
-    const { id, password } = req.body;
-    const user = await User.findOne({ id });
-
-    if (!user) {
-      return res
-        .status(401)
-        .json({ message: '아이디 또는 비밀번호가 일치하지 않습니다.' });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return res
-        .status(401)
-        .json({ message: '아이디 또는 비밀번호가 일치하지 않습니다.' });
-    }
-
-    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
-      expiresIn: '1h',
-    });
-
-    const userInfo = {
-      id: user.id,
-      username: user.username,
-      group: user.group,
-    };
-
-    res.status(200).json({ message: '로그인 성공', token, userInfo });
-  } catch (error) {
-    res.status(500).json({ message: '로그인 실패', error: error.message });
-  }
-});
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
