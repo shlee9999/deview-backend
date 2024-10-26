8962324617c23f8fe72d455899eccb6663d2e1b1
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-26 09:22:54 +0900

    Feat: postMiddleware 분리

---
+++ middleware/postMiddleware.js
@@ -0,0 +1,26 @@
+const Post = require('../models/Post');
+
+exports.isPostAuthor = async (req, res, next) => {
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
+    if (post.author.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: 'Not authorized to modify this post' });
+    }
+
+    req.post = post;
+    next();
+  } catch (error) {
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+      return res.status(400).json({ message: 'Invalid Post ID' });
+    }
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,10 +2,21 @@ const express = require('express');
 const router = express.Router();
 const jwtMiddleware = require('../middleware/jwtMiddleware');
 const postController = require('../controllers/postController');
+const postMiddleware = require('../middleware/postMiddleware');
 
 router.get('/', postController.getAllPosts);
 router.post('/', jwtMiddleware, postController.createPost);
-router.put('/:id', jwtMiddleware, postController.updatePost);
-router.delete('/:id', jwtMiddleware, postController.deletePost);
+router.put(
+  '/:id',
+  jwtMiddleware,
+  postMiddleware.isPostAuthor,
+  postController.updatePost
+);
+router.delete(
+  '/:id',
+  jwtMiddleware,
+  postMiddleware.isPostAuthor,
+  postController.deletePost
+);
 
 module.exports = router;
