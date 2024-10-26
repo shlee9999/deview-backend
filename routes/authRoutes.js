3b8e8f329121ae2913ccb857b223439ac5dc9ca9
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-26 15:53:48 +0900

    Feat: auth/myself

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+const jwtMiddleware = require('../middleware/jwtMiddleware');
+router.get('/myself', jwtMiddleware, authController.myself);
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/refresh', authController.refreshToken);
