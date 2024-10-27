492c8136b58170d992047c2942a91f3858692282
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-27 14:33:07 +0900

    Refactor: app.js

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/database');
-const cookieParser = require('cookie-parser');
-
-//Middlewares
-const corsMiddleware = require('./middleware/corsMiddleware');
-
-// Routes
-const authRoutes = require('./routes/authRoutes');
-const postRoutes = require('./routes/postRoutes');
-const commentRoutes = require('./routes/commentRoutes');
+const setupMiddlewares = require('./middleware');
+const setupRoutes = require('./routes');
+const { port } = require('./config/server');
 
 const app = express();
-const port = 5000;
 
+// 데이터베이스 연결
 connectDB(process.env.MONGODB_URI);
 
-app.use(corsMiddleware);
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// 미들웨어 설정
+setupMiddlewares(app);
 
-app.get('/', (req, res) => {
-  res.json('welcome to DEVIEW server');
-});
-
-app.use('/auth', authRoutes);
-app.use('/post', postRoutes);
-app.use('/comment', commentRoutes);
+// 라우트 설정
+setupRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
