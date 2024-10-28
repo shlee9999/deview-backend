3f098e89c869f43500721574e89dcf0713e4d1c7
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-28 09:24:39 +0900

    Feat: jwt 관련 에러 처리 개선

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -1,20 +1,34 @@
 const jwt = require('jsonwebtoken');
 
-function jwtMiddleware(req, res, next) {
-  const token = req.headers.authorization?.split(' ')[1];
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1]; // Bearer TOKEN
 
   if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
+    return res.status(403).json({ message: '토큰이 제공되지 않았습니다.' });
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({
+          message: '토큰이 만료되었습니다. 다시 로그인해 주세요.',
+          error: 'TOKEN_EXPIRED',
+        });
+      } else if (err.name === 'JsonWebTokenError') {
+        return res.status(401).json({
+          message: '유효하지 않은 토큰입니다.',
+          error: 'INVALID_TOKEN',
+        });
+      }
+      return res.status(401).json({
+        message: '인증에 실패했습니다.',
+        error: 'AUTH_FAILED',
+      });
     }
 
     req.user = decoded;
     next();
   });
-}
+};
 
-module.exports = jwtMiddleware;
+module.exports = verifyToken;
