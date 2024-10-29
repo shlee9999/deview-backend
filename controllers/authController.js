12d2800afff09443cee60352a52f395439ec25e5
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2024-10-29 17:27:03 +0900

    Fix: cookie 옵션 secure false

--- controllers/authController.js
+++ controllers/authController.js
@@ -86,7 +86,7 @@ exports.login = async (req, res) => {
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      //! secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
